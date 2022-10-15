a5b61c599814f518fbb846c3936923590001742c
LQJ233 <75492760+LQJ233@users.noreply.github.com>
2022-10-15 17:09:11 +0800

    Delete OpenCV directory

--- OpenCV/图像金字塔.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-import numpy as np
-import matplotlib.pyplot as plt
-img=cv.imread('C:\\Users\\ASUS\\Desktop\\222.jpg',1)
-#img1=cv.pyrUp(img)向上采样，图片更大，更清晰
-img1=cv.pyrDown(img)#图像更小，
-cv.imshow('show',img1)
-cv.waitKey(0)
-cv.destroyAllWindows()
